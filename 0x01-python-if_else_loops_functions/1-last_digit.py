0394742aab84a570e2d7d57c2372f5a7a87cb716
Dev-Kings <kingasiadavid41@gmail.com>
2023-11-30 06:46:14 -0800

    added whitespace

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -6,12 +6,12 @@ if number < 0:
     last_digit = number % 10
     if last_digit > 5:
         print(f"Last digit of -{number} is -{last_digit} and is less than\
-6 and not 0")
+ 6 and not 0")
     elif last_digit == 0:
         print(f"Last digit of -{number} is {last_digit} and is 0")
     else:
         print(f"Last digit of -{number} is -{last_digit} and is less than\
-6 and not 0")
+ 6 and not 0")
 else:
     last_digit = number % 10
     if last_digit > 5:

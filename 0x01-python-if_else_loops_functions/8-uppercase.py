a1a8a02cc7536b2549b8e79b52c8daabf8cae642
Dev-Kings <kingasiadavid41@gmail.com>
2023-12-01 07:01:56 -0800

    removed extra print()

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,10 +1,20 @@
 #!/usr/bin/python3
+#def uppercase(str):
+ #   for c in str:
+  #      if ord(c) > 96 and ord(c) < 123:
+   #         print("{}".format(chr(ord(c) - 32)), end='')
+    #    elif ord(c) > 64 and ord(c) < 91:
+     #       print("{}".format(chr(ord(c))), end='')
+      #  else:
+       #     print("{}".format(c), end='')
+   # print()
+
 def uppercase(str):
+    result = ""
     for c in str:
-        if ord(c) > 96 and ord(c) < 123:
-            print("{}".format(chr(ord(c) - 32)), end='')
-        elif ord(c) > 64 and ord(c) < 91:
-            print("{}".format(chr(ord(c))), end='')
+        if 'a' <= c <= 'z':
+            result += chr(ord(c) - ord('a') + ord('A'))
         else:
-            print("{}".format(c), end='')
+            result += c
+    print(result, end='')
     print()

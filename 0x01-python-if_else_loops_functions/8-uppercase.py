0e11b3c064877c7bbbb2a39f1b5864929ca0b045
Dev-Kings <kingasiadavid41@gmail.com>
2023-12-01 06:30:04 -0800

    changed format

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -2,9 +2,9 @@
 def uppercase(str):
     for c in str:
         if ord(c) > 96 and ord(c) < 123:
-            print(chr(ord(c) - 32), end='')
+            print("{}".format(chr(ord(c) - 32)), end='')
         elif ord(c) > 64 and ord(c) < 91:
-            print(chr(ord(c)), end='')
+            print("{}".format(chr(ord(c))), end='')
         else:
-            print(c, end='')
+            print("{}".format(c), end='')
     print()

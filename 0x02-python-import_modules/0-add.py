7274a9903e8dab7a973611c2272f478cdc6cb459
Dev-Kings <kingasiadavid41@gmail.com>
2023-12-05 05:30:14 -0800

    refactored

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-import add_0 as add
 a = 1
 b = 2
-summation = add.add(a, b)
-print("{:d} + {:d} = {:d}".format(a, b, summation))
+
+from add_0 import add
+
+print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))

bb7e57aa830c92838eeb9c52e450e8f24b65efec
Dev-Kings <kingasiadavid41@gmail.com>
2023-12-05 23:22:52 -0800

    removed unwanted print

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -10,7 +10,6 @@ if __name__ == "__main__":
         a = int(sys.argv[1])
         b = int(sys.argv[3])
         operator = sys.argv[2]
-        print("{:s}".format(sys.argv[2]))
         if sys.argv[2] in ['+', '-', '*', '/']:
             if operator == '+':
                 result = add(a, b)

95afe265f9c7ac98fe3c41c373c56c5683023877
Dev-Kings <kingasiadavid41@gmail.com>
2023-12-07 03:56:37 -0800

    indented properly

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -4,6 +4,6 @@ def replace_in_list(my_list, idx, element):
     if idx < 0 or idx >= list_length:
         return (my_list)
     else:
-       del(my_list[idx])
-       my_list.insert(idx, element)
-       return (my_list)
+        del(my_list[idx])
+        my_list.insert(idx, element)
+        return (my_list)

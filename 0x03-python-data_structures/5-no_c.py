c86eacedaf5cac8c300cff6ea73d136814067116
Dev-Kings <kingasiadavid41@gmail.com>
2023-12-07 05:17:24 -0800

    removed multiple spaces

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -3,7 +3,7 @@ def no_c(my_string):
     string_list = []
     new_str = ""
     for s in my_string:
-        if s ==  'c' or s == 'C':
+        if s == 'c' or s == 'C':
             continue
         else:
             string_list.append(s)

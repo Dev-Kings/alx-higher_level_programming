b570da109104fd8aef19e02fc6ba0490c58ff031
Dev-Kings <kingasiadavid41@gmail.com>
2023-12-11 00:49:29 -0800

    changed for to list comprehension

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,8 +1,3 @@
 def square_matrix_simple(matrix=[]):
-    outer_matrix = []
-    for row in matrix:
-        new_list = []
-        for i in row:
-            new_list.append(i ** 2)
-        outer_matrix.append(new_list)
-    return (outer_matrix)
+    new_matrix = [[x**2 for x in row] for row in matrix]
+    return (new_matrix)

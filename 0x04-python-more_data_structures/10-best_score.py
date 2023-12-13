6a5e8e2da7c2a89b637d5854a2e68f5c0b5d03d0
Dev-Kings <kingasiadavid41@gmail.com>
2023-12-13 11:24:17 -0800

    added case for empty dictionary

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -2,7 +2,7 @@
 def best_score(a_dictionary):
     temp = 0
     max_key = ''
-    if a_dictionary is None:
+    if a_dictionary is None or len(a_dictionary) == 0:
         return (None)
     else:
         for key in a_dictionary:

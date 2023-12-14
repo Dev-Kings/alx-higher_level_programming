62f6645456d619849c6cf99a6b8d559448e7dfd0
Dev-Kings <kingasiadavid41@gmail.com>
2023-12-14 00:30:24 -0800

    removed import, used for loop and reversed string, used dict

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,9 +1,20 @@
 #!/usr/bin/python3
-from roman import fromRoman
-
-
 def roman_to_int(roman_string):
     if type(roman_string) is not str or roman_string is None:
         return (0)
     else:
-        return (fromRoman(roman_string))
+        roman_numerals = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100,
+                'D': 500, 'M': 1000}
+        result = 0
+        prev_value = 0
+
+        for num in reversed(roman_string):
+            value = roman_numerals[num]
+
+            if value < prev_value:
+                result -= value
+            else:
+                result += value
+
+            prev_value = value
+    return (result)

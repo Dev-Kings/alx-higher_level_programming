41465187a663f07ee0cabda448ec7f2d8b063516
Dev-Kings <kingasiadavid41@gmail.com>
2024-01-26 05:03:48 -0800

    minor doc changes

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 class Square:
-	"""This is an empty class at the moment.
-	"""
-	pass
+    """This is an empty class at the moment.
+    The __init__ method has no arguments currently.
+    """
+    def __init__(self):
+        pass

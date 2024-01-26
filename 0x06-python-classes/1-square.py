d9eb981254120872d94fcbcfea7655c7255afea6
Dev-Kings <kingasiadavid41@gmail.com>
2024-01-26 05:23:00 -0800

    class with private instance attribute

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+A module initializing class Square which defines a square.
+"""
+
+
+class Square:
+    """This is an empty class at the moment.
+    The __init__ method has no arguments currently.
+
+    Args:
+        size (str): Size of square.
+    """
+    def __init__(self, size):
+        self.__size = size

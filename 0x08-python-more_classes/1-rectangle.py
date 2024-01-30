7a1543a2d1bb662cc4f08d9f599e96931a1b0b68
Dev-Kings <kingasiadavid41@gmail.com>
2024-01-30 07:04:27 -0800

    added validation to init

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -13,8 +13,17 @@ class Rectangle:
             width (int, optional): Width of rectangle.
             height (int, optional): Height of rectangle.
         """
-        self.__width = width
-        self.__height = height
+        if not isinstance(width, int):
+            raise TypeError("width must be an integer")
+        elif not isinstance(height, int):
+            raise TypeError("height must be an integer")
+        elif width < 0:
+            raise ValueError("width must be >= 0")
+        elif height < 0:
+            raise ValueError("height must be >= 0")
+        else:
+            self.__width = width
+            self.__height = height
 
     @property
     def width(self):
@@ -48,6 +57,7 @@ class Rectangle:
         if not isinstance(value, int):
             raise TypeError("height must be an integer")
         elif value < 0:
+            print("{} <= 0".format(value))
             raise ValueError("height must be >= 0")
         else:
             self.__height = value

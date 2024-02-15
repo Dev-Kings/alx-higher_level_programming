ac9f1138e1fd8d4e686c56c08d21f6b5793b57c5
Dev-Kings <kingasiadavid41@gmail.com>
2024-02-15 05:13:41 -0800

    handled no argument provided case

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -12,6 +12,8 @@ def say_my_name(first_name, last_name=""):
     Raises:
         TypeError: if first_name or last_name is not a string.
     """
+    if first_name is None:
+        print("No first name")
     if not isinstance(first_name, str):
         raise TypeError("first_name must be a string")
     if not isinstance(last_name, str):

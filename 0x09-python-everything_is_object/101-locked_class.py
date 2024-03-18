359927221140434c76b789f04dec09d1c678d0bc
Dev-Kings <kingasiadavid41@gmail.com>
2024-03-18 09:42:29 -0700

    custom override of setattr built-in

---
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -0,0 +1,20 @@
+"""A module with LockedClass class."""
+
+
+class LockedClass:
+    """The class has setattr method.
+    """
+
+    def __setattr__(self, name, value=None):
+        """An override of setattr built-in.
+        Args:
+            name (string): instance attribute.
+            value (string, optional): value to set.
+        Raises:
+            AttributeError: if name is not 'first_name'
+        """
+        if name != 'first_name':
+            raise AttributeError("'LockedClass' object has no attribute {}"
+                                 .format(name))
+        else:
+            self.__dict__['name'] = value

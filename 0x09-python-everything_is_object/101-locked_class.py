88bfa60c555f1cdd30193f9350f6e5c5b0727dc7
Dev-Kings <kingasiadavid41@gmail.com>
2024-04-09 08:02:36 -0700

    cleaned code

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -6,7 +6,7 @@ class LockedClass:
     """The class has setattr method.
     """
 
-    def __setattr__(self, name="first_name", value=None):
+    def __setattr__(self, name, value=None):
         """An override of setattr built-in.
         Args:
             name (string): instance attribute.
@@ -18,8 +18,4 @@ class LockedClass:
             raise AttributeError("'LockedClass' object has no attribute '{}'"
                                  .format(name))
         else:
-            self.__dict__[name] = value
-
-    def __dict__(self):
-        """Returns a dictionary of instance attributes."""
-        return (self.__dict__)
+            self.__dict__['first_name'] = value
--- 0x09-python-everything_is_object/101-main.py
+++ 0x09-python-everything_is_object/101-main.py
@@ -4,7 +4,6 @@ LockedClass = __import__('101-locked_class').LockedClass
 lc = LockedClass()
 lc.first_name = "John"
 try:
-    print(lc.last_name)
     lc.last_name = "Snow"
 except Exception as e:
     print("[{}] {}".format(e.__class__.__name__, e))

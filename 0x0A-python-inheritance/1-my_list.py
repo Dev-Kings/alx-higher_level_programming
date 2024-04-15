d4e62086e8e5da9074da375696b4c47ffb8b85fb
Dev-Kings <kingasiadavid41@gmail.com>
2024-04-15 23:42:24 +0300

    added 3 to python

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,9 +1,11 @@
-#!/usr/bin/python
-"""Module with MyList class"""
+#!/usr/bin/python3
+"""Module with MyList class. MyList class inherits from list super class."""
 
 
 class MyList(list):
-    """Class inherits from super class list."""
+    """Class inherits from super class list. The class then implements
+    printing of list in ascending order.
+    """
 
     def print_sorted(self):
         """Custom function to print sorted list containing integers."""

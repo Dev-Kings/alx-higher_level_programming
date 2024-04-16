bb3d2b1571c268f2324cc1ec66619792178c6fc5
Dev-Kings <kingasiadavid41@gmail.com>
2024-04-16 21:50:14 +0300

    function to read file

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Module with read_file function."""
+
+
+def read_file(filename=""):
+    """This function reads a text file with UTF8 encoding."""
+    with open('my_file_0.txt', encoding='utf-8') as my_file:
+        for line in my_file:
+            print(line, end='')

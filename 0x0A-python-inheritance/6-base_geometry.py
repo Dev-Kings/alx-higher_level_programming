751b9917732f5f0ddc34b1741fffbba129551a1c
Dev-Kings <kingasiadavid41@gmail.com>
2024-04-17 19:10:15 +0300

    function that raises an Exception

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+BaseGeometry class.
+"""
+
+
+class BaseGeometry:
+    """
+    Contains area function.
+    """
+
+    def area(self):
+        """
+        Raises:
+            Exception if area is not implemented.
+        """
+        raise Exception("area() is not implemented")

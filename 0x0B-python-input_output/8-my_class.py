764cd41b2055cd296b4b7411211781eb487e9185
Dev-Kings <kingasiadavid41@gmail.com>
2024-04-17 10:54:31 +0300

    wrote a class Student

--- 0x0B-python-input_output/8-my_class.py
+++ 0x0B-python-input_output/8-my_class.py
@@ -9,6 +9,12 @@ class MyClass:
     def __init__(self, name):
         self.name = name
         self.number = 0
+        self.matrix = [
+                    [1, 2, 3],
+                    [4, 5, 6]
+                ]
+        self.my_tup = (3, 5, 6, 9)
+        self.my_set = {'str', 54, (3, 6), 3.2}
 
     def __str__(self):
         return "[MyClass] {} - {:d}".format(self.name, self.number)

db70652c0fe15aadd16d1f28806f27079a903722
Dev-Kings <kingasiadavid41@gmail.com>
2024-04-17 19:30:19 +0300

    function to check type of value

---
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""
+BaseGeometry class.
+"""
+
+
+class BaseGeometry:
+    """
+    Contains area and integer_validator functions.
+    """
+
+    def area(self):
+        """
+        Raises:
+            Exception if area is not implemented.
+        """
+        raise Exception("area() is not implementeid")
+
+    def integer_validator(self, name, value):
+        """
+        Validates value.
+        Args:
+            name: (string): name of value.
+            value: (int): value of name.
+        Raises:
+            TypeError: if value is not an integer.
+            ValueError: if value is less or equal to 0.
+        """
+        if type(value) is not int:
+            raise TypeError("{} must be an integer".format(name))
+        if int(value) <= 0:
+            raise ValueError("{} must be greater than 0".format(name))

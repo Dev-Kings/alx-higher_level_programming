af4a7e739a9ad5764d522fed7f0a20ac04981708
Dev-Kings <kingasiadavid41@gmail.com>
2024-04-22 20:32:47 +0300

    removed extra lines unused

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -1,12 +1,7 @@
 #!/usr/bin/python3
 """ Unittest module for base.py """
 import unittest
-import sys
-import os
 
-current_dir = os.path.dirname(os.path.realpath(__file__))
-parent_dir = os.path.abspath(os.path.join(current_dir, '..', '..'))
-sys.path.append(parent_dir)
 from models.base import Base
 from models.rectangle import Rectangle
 from models.square import Square

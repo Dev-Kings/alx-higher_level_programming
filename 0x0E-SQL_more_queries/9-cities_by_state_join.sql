88b1d55562ae20ba5b5ca5b1c7ab1df5ff7313e8
Dev-Kings <kingasiadavid41@gmail.com>
2024-05-23 00:48:48 +0300

    added inner join

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,6 +1,6 @@
 -- Lists all cities in database hbtn_0d_usa
 -- Use one SELECT element.
-SELECT cities.id AS id, cities.name AS name, states.name AS name,
+SELECT cities.id, cities.name, states.name,
 FROM cities
 INNER JOIN states ON cities.state_id = state.id
 ORDER BY cities.id ASC;

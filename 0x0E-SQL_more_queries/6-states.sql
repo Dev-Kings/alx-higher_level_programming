b8f5c9c028c5333427998a6e28e0e4147963587b
Dev-Kings <kingasiadavid41@gmail.com>
2024-05-22 23:55:26 +0300

    primary key, auto increment id refactored

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -2,7 +2,8 @@
 -- In the database hbtn_0d_usa
 -- id unique, auto incremented, not null, primary key
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
-CREATE TABLE IF NOT EXISTS hbtn_0d_usa.states(
+USE hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS `states` (
 	id INT NOT NULL AUTO INCREMENT PRIMARY KEY,
 	name VARCHAR(256),
 	);

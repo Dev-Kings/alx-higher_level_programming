ee912082ff419c96e87d84ee0ee6b3770aa36d1e
Dev-Kings <kingasiadavid41@gmail.com>
2024-05-23 00:34:34 +0300

    select from table based on foreign id on another table cleaned

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,6 +1,7 @@
 -- Lists all cities California on database hbtn_0d_usa
 -- Only cities of California state.
-SELECT * FROM cities
-WHERE cities.state_id = (
+SELECT id, name FROM cities
+WHERE cities.state_id IN (
 	SELECT id FROM states WHERE name = "California"
+ORDER BY id ASC;
 );

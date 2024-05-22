ecb67e8e0d66128e63eeb9890b7fe88f2a984653
Dev-Kings <kingasiadavid41@gmail.com>
2024-05-23 01:16:28 +0300

    inner join and group by

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -5,9 +5,9 @@
 -- Don’t display a genre that doesn’t have any shows linked
 -- Results must be sorted in descending order by the number of shows linked
 -- You can use only one SELECT statement
-SELECT tv_shows.name AS genre, COUNT(*) AS number_of_shows
-FROM tv_shows
+SELECT tv_genres.name AS genre, COUNT(*) AS number_of_shows
+FROM tv_genres
 INNER JOIN tv_show_genres
-ON tv_shows.id = tv_show_genres.genre_id
+ON tv_genres.id = tv_show_genres.genre_id
 GROUP BY tv_genres.name
 ORDER BY number_of_shows DESC;

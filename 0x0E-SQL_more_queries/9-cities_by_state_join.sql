a24424804ceddb1c6b1ecc7a79431a817a0c8915
Dev-Kings <kingasiadavid41@gmail.com>
2024-05-23 00:55:26 +0300

    removed ,

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,6 +1,6 @@
 -- Lists all cities in database hbtn_0d_usa
 -- Use one SELECT element.
-SELECT cities.id, cities.name, states.name,
+SELECT cities.id, cities.name, states.name
 FROM cities
 LEFT JOIN states ON states.id = cities.state_id
 ORDER BY cities.id;

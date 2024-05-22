834bf8c63da22fda06346b9fa7c7a84349acf768
Dev-Kings <kingasiadavid41@gmail.com>
2024-05-23 01:38:45 +0300

    select from inner join on group by order by

---
+++ 0x0E-SQL_more_queries/102-rating_shows.sql
@@ -0,0 +1,10 @@
+-- lists all shows from hbtn_0d_tvshows_rate by their rating.
+-- Each record should display: tv_shows.title - rating sum
+-- Results must be sorted in descending order by the rating
+-- You can use only one SELECT statement
+SELECT `title`, SUM(`rate`) AS `rating`
+FROM `tv_shows` AS ts
+INNER JOIN `tv_show_ratings` AS tsr
+ON ts.`id` = tsr.`show_id`
+GROUP BY `title`
+ORDER BY `rating` DESC;

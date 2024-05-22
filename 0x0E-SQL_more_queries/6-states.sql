8903b8fb16cd095e0c2b22d046f4da79893ca2ce
Dev-Kings <kingasiadavid41@gmail.com>
2024-05-22 23:47:05 +0300

    primary key, auto increment id

---
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -0,0 +1,9 @@
+-- creates database hbtn_0d_usa and table states
+-- In the database hbtn_0d_usa
+-- id unique, auto incremented, not null, primary key
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS `states`(
+	id INT NOT NULL AUTO_INCREMENT,
+	name VARCHAR(256),
+	PRIMARY KEY(id)
+	);

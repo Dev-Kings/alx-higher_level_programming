5db521103871bbd629349e997dafdf70497775e9
Dev-Kings <kingasiadavid41@gmail.com>
2024-05-23 00:52:46 +0300

    added inner join

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -2,5 +2,5 @@
 -- Use one SELECT element.
 SELECT cities.id, cities.name, states.name,
 FROM cities
-LEFT JOIN states ON states.id = cities.state_id
+LEFT JOIN states ON cities.state_id = states.id
 ORDER BY cities.id ASC;

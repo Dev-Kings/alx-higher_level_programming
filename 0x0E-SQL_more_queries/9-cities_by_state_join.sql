6bbcbc79137b0b1acc091a8ba62b0025be7689a6
Dev-Kings <kingasiadavid41@gmail.com>
2024-05-23 00:45:39 +0300

    added inner join

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -4,5 +4,5 @@ SELECT cities.id AS id,
 cities.name AS name,
 states.name AS name,
 FROM cities
-JOIN states ON cities.state_id = state.id
+INNER JOIN states ON cities.state_id = state.id
 ORDER BY cities.id ASC;

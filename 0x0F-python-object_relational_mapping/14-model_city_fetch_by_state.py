d4c2689ccb740c289456a19a7cd08add49136c67
Dev-Kings <kingasiadavid41@gmail.com>
2024-06-23 22:00:52 +0300

    Prints City objects from db, and their State name

---
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+"""
+Script that prints all City objects from the database hbtn_0e_14_usa
+"""
+
+import sys
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+from model_city import City
+
+if __name__ == "__main__":
+    uname = sys.argv[1]
+    pwd = sys.argv[2]
+    db = sys.argv[3]
+
+    # Create engine to connect to MySQL server
+    engine = create_engine(
+            'mysql+mysqldb://{}:{}@localhost:3306/{}'.format(uname, pwd, db))
+
+    # Bind Base class to the engine
+    Base.metadata.create_all(engine)
+
+    # Create a configured "Session" class
+    Session = sessionmaker(bind=engine)
+
+    # Create a Session
+    session = Session()
+
+    # Query City objects, sorted by id, with state name included
+    cities = session.query(City).join(State).order_by(City.id).all()
+
+    # Print the results
+    for city in cities:
+        print("{}: ({}) {}".format(city.state.name, city.id, city.name))
+
+    # Close the session
+    session.close()
---
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+Module contains the class definition of a City class and an instance
+Base = declarative_base(). The City class definition is mapped to the cities
+table in MySQL database.
+"""
+from sqlalchemy import Column, Integer, String, ForeignKey
+from model_state import Base
+
+
+class City(Base):
+    """
+    City class that links to the MySQL table 'cities'.
+    """
+    __tablename__ = 'cities'
+    id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
+    name = Column(String(128), nullable=False)
+    state_id = Column(Integer, ForeignKey('states.id'), nullable=False)

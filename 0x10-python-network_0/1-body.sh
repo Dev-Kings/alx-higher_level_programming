0d01b7db5e140606d672d95b813191e75d28ead9
Dev-Kings <kingasiadavid41@gmail.com>
2024-07-05 00:03:11 +0300

    refactored

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Send a GET request to URL, status code is 200, display the body of response
-curl -s -w "%{http_code}" "$1" | { read body; read code; [ "$code" -eq 200 ] && echo "$body"; }
+curl -sL "$1"

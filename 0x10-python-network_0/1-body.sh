28d57f505ef4d5daeb0584bd995c97a892c20ade
Dev-Kings <kingasiadavid41@gmail.com>
2024-07-04 23:15:13 +0300

    script to send a DELETE request to URL

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -2,10 +2,10 @@
 # Send a GET request to the provided URL
 # If the status code is 200, display the body of the response
 curl -s -o /dev/stdout -w "%{http_code}" "$1" | {
-	  read -r body
-	  read -r code
-    if [ "$code" -eq 200 ]; then
-	    echo "$body"
-    fi
+	read -r body
+	read -r code
+	if [ "$code" -eq 200 ]; then
+		echo "$body"
+	fi
 }
 
---
+++ 0x10-python-network_0/2-delete.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+# Sends a DELETE request to the provided URL and display the body of the response
+curl -s -X DELETE "$1"

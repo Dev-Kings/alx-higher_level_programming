d00e1b99569dc6ad420a87cfadc627dc76f4dcf6
Dev-Kings <kingasiadavid41@gmail.com>
2024-07-04 23:09:32 +0300

    script that takes URL and displays body of response if status is 200

---
+++ 0x10-python-network_0/1-body.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# Send a GET request to the provided URL
+# If the status code is 200, display the body of the response
+curl -s -o /dev/stdout -w "%{http_code}" "$1" | {
+	  read -r body
+	  read -r code
+    if [ "$code" -eq 200 ]; then
+	    echo "$body"
+    fi
+}
+

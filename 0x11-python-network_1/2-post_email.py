0da85655ca2c43ad36e1dd2b57c9ee353ce66348
Dev-Kings <kingasiadavid41@gmail.com>
2024-07-06 14:42:17 +0300

    ref3

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -11,7 +11,7 @@ import sys
 if __name__ == "__main__":
     url = sys.argv[1]
     email = sys.argv[2]
-    data = urllib.parse.urlencode({'email': email}).encode('utf-8')
+    data = urllib.parse.urlencode({'email': email})
     data = data.encode('ascii') # Data is in bytes
 
     req = urllib.request.Request(url, data)

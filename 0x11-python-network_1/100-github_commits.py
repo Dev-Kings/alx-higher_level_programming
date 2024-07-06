1314f57d6ec49c1f41dfa18bf642a02879b234dd
Dev-Kings <kingasiadavid41@gmail.com>
2024-07-06 23:05:34 +0300

    ref4

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -12,7 +12,7 @@ import sys
 if __name__ == "__main__":
     repo = sys.argv[1]
     owner = sys.argv[2]
-    url = 'https://api.github.com/repos/{owner}/{repo}/commits'
+    url = 'https://api.github.com/repos/{}/{}/commits'.format(owner, repo)
 
     response = requests.get(url)
 

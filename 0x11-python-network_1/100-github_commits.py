3fe184065dfb44f1575b328477078b56f7c8b6f1
Dev-Kings <kingasiadavid41@gmail.com>
2024-07-06 22:55:25 +0300

    script that lists commits of repo in Github

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+This script takes 2 arguments (repository name and owner name)
+and lists the 10 most recent commits of the repository by the user
+using the GitHub API.
+Prints all commits by: <sha>: <author name> (one per line).
+"""
+
+import requests
+import sys
+
+if __name__ == "__main__":
+    repo = sys.argv[1]
+    owner = sys.argv[2]
+    url = 'https://api.github.com/repos/{owner}/{repo}/commits'
+
+    response = requests.get(url)
+    commits = response.json()
+
+    for commit in commits[:10]:
+        sha = commit.get('sha')
+        author = commit.get('commit').get('author').get('name')
+        print(f"{sha}: {author}")
